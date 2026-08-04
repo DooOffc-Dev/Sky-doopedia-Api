d44fcd1be30219d9d3896c34de1de73b325073ab
Xsky_DooCodex <saparipari144@gmail.com>
2026-08-04 17:34:46 +0700

    Fix formatting in index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const settings = {
   linkWhatsapp: "https://t.me/Xsky_doopedia",
   linkChannel: "https://whatsapp.com/channel/0029VbDThaa7tkj1ZTGzfw3X", 
   linkGithub: "https://github.com/Xsky_DooCodex", 
-  linkyoutube: "https://www.youtube.com/@sky_doo",
+  linkyoutube: "https://www.youtube.com/@sky_doo"
 };
 
 global.apikey = ["123", "1"]
